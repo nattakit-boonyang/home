03273438cd8ab383fb910a46372fbcfe12209180
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2023-10-04 02:40:10 +1000

    Update fist config

--- fish/config.fish
+++ fish/config.fish
@@ -5,7 +5,7 @@ source $HOME/.iterm2_shell_integration.fish
 set -Ux EDITOR nvim
 
 # empty greeting
-set -U fish_greeting
+set -U fish_greeting ""
 
 # append PATH
 set PATH $PATH /usr/local/bin
@@ -16,9 +16,11 @@ set PATH $PATH /usr/local/opt/whois/bin
 set PATH $PATH /usr/local/sbin
 set PATH $PATH /usr/local/bin
 set PATH $PATH $HOME/.local/bin
+set PATH $PATH $HOME/.nvim-nightly/bin
+set MANPATH $HOME/.nvim-nightly/share/main
 
 # custom pure themes
-set --universal pure_symbol_prompt ""
+set --universal pure_symbol_prompt ""
 set --universal pure_show_system_time false
 set --universal pure_enable_k8s false
 set --universal pure_shorten_prompt_current_directory_length 1
@@ -33,6 +35,7 @@ fzf_configure_bindings --history=\cr --directory=\cf --variables= --processes= -
 
 # alias ls/exa
 alias ls="exa --color=always"
+alias l="ls"
 
 # alias tmux
 alias td="tmux detach"
@@ -40,8 +43,24 @@ alias ta="tmux attach"
 alias tsd="tmux new-session -d -t"
 
 # alias shell
+alias fish_reload="source $HOME/.config/fish/config.fish"
 alias jl="jobs -l"
-alias v="vim"
+alias vim="nvim"
+alias v="nvim"
 alias e="exit"
 alias c="clear"
+alias zz="zellij"
 
+# delete git abbr
+abbr --erase gup
+
+# often use
+function new_abbr
+  abbr --add $argv[1] --position anywhere $argv[2]
+end
+
+new_abbr gozerolog "github.com/rs/zerolog"
+new_abbr gotestify "github.com/stretchr/testify"
+new_abbr gogin "github.com/gin-gonic/gin"
+new_abbr goecho "github.com/labstack/echo/v4"
+new_abbr gofiber "github.com/gofiber/fiber/v2"
