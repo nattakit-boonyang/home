a1eaf85d5a4f39b5ca483b9a96dbe3c0b543bc0e
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2024-06-18 00:04:50 +0700

    Rewrite fish config to support multiple os

--- fish/config.fish
+++ fish/config.fish
@@ -1,28 +1,58 @@
-# iterm2 shell integration
-source $HOME/.iterm2_shell_integration.fish
-
-# editor: nvim
 set -Ux EDITOR nvim
-
-# empty greeting
 set -U fish_greeting ""
 
-# append PATH
-set PATH $HOME/.bin $PATH
+# set default PATH
 set PATH $PATH /usr/local/bin
-set PATH $PATH $HOME/go/bin 
-set PATH $PATH /usr/local/opt/openjdk/bin
-set PATH $PATH /usr/local/opt/mysql-client/bin
-set PATH $PATH /usr/local/opt/whois/bin
+set PATH $PATH $HOME/go/bin
 set PATH $PATH /usr/local/sbin
 set PATH $PATH /usr/local/bin
 set PATH $PATH $HOME/.local/bin
-set PATH $PATH $HOME/.spicetify
+
+# set default alias
+alias fish_reload="source $HOME/.config/fish/config.fish"
+alias ls="exa -g --color=always"
+alias l="ls"
+alias e="exit"
+alias c="clear"
+alias vim="nvim"
+alias v="nvim"
+alias vv="nvim ."
+
+function otherwise
+    # set PATH
+    set PATH /usr/local/go/bin $PATH
+    # alias
+    alias pbcopy="xsel -b --input"
+    alias pbpaste="xsel -b --output"
+    if test -f /usr/bin/wslview
+        alias ff="/mnt/c/Program\ Files/Mozilla\ Firefox/firefox.exe -p"
+    end
+end
+
+function only_darwin
+    # iterm2 shell integration
+    source $HOME/.iterm2_shell_integration.fish
+    # set PATH
+    set PATH $PATH /usr/local/opt/openjdk/bin
+    set PATH $PATH /usr/local/opt/mysql-client/bin
+    set PATH $PATH /usr/local/opt/whois/bin
+    set PATH $PATH $HOME/.spicetify
+    # alias
+    alias ff="open -a Firefox -n --args -p"
+end
+
+# select os
+switch (uname)
+    case Darwin
+        only_darwin
+    case '*'
+        otherwise
+end
 
 # custom pure themes
-set --universal pure_symbol_prompt ""
-set --universal pure_show_system_time false
-set --universal pure_shorten_prompt_current_directory_length 1
+set -U pure_symbol_prompt ""
+set -U pure_show_system_time false
+set -U pure_shorten_prompt_current_directory_length 1
 
 # custom tmux
 set -Ux fish_tmux_autostart false
@@ -32,18 +62,8 @@ set -Ux fish_tmux_autoconnect false
 # custom bindings for fzf
 fzf_configure_bindings --history=\cr --directory=\cf --variables= --processes= --git_log= --git_status=
 
-# alias ls/exa
-alias ls="exa -g --color=always"
-alias l="ls"
-
-# alias shell
-alias fish_reload="source $HOME/.config/fish/config.fish"
-alias vim="nvim"
-alias v="nvim"
-alias vv="nvim ."
-alias e="exit"
-alias c="clear"
-alias ff="open -a Firefox -n --args -p"
+# delete git abbr
+abbr --erase gup
 
 # abbreviation last history
 function last_history_item
@@ -51,40 +71,47 @@ function last_history_item
 end
 abbr --add !! --position anywhere --function last_history_item
 
-# abbreviation tmux
-abbr --add td "tmux detach"
-abbr --add ta "tmux attach"
-abbr --add tsd "tmux new-session -d -t"
-
-# abbreviation docker
-abbr --add dcu "docker compose up -d"
-abbr --add dcd "docker compose down"
-abbr --add dcp "docker compose ps -a"
-abbr --add dcl "docker compose logs -f"
-
-# delete git abbr
-abbr --erase gup
-
 # often use
 function new_abbr
-  abbr --add $argv[1] --position anywhere $argv[2]
+    abbr --add $argv[1] --position anywhere $argv[2]
 end
 
-new_abbr go_zerolog   "github.com/rs/zerolog"
-new_abbr go_testify   "github.com/stretchr/testify"
-new_abbr go_gin       "github.com/gin-gonic/gin"
-new_abbr go_echo      "github.com/labstack/echo/v4"
-new_abbr go_fiber     "github.com/gofiber/fiber/v2"
-new_abbr go_fasthttp  "github.com/valyala/fasthttp"
-new_abbr go_kafka_v2  "github.com/confluentinc/confluent-kafka-go/v2"
-new_abbr go_dotenv    "github.com/joho/godotenv"
-new_abbr go_envconfig "github.com/kelseyhightower/envconfig"
-new_abbr go_mongo     "go.mongodb.org/mongo-driver/mongo"
-new_abbr go_resty     "github.com/go-resty/resty/v2"
-new_abbr go_goquery   "github.com/PuerkitoBio/goquery"
+# abbreviation tmux
+new_abbr td "tmux detach"
+new_abbr ta "tmux attach"
+new_abbr tsd "tmux new-session -d -t"
 
+# abbreviation docker
+new_abbr dcu "docker compose up -d"
+new_abbr dcd "docker compose down"
+new_abbr dcp "docker compose ps -a"
+new_abbr dcl "docker compose logs -f"
+
+# abbreviation go package frequency used
+new_abbr go_zerolog "github.com/rs/zerolog"
+new_abbr go_testify "github.com/stretchr/testify"
+new_abbr go_gin "github.com/gin-gonic/gin"
+new_abbr go_echo "github.com/labstack/echo/v4"
+new_abbr go_fiber "github.com/gofiber/fiber/v2"
+new_abbr go_fasthttp "github.com/valyala/fasthttp"
+new_abbr go_kafka_v2 "github.com/confluentinc/confluent-kafka-go/v2"
+new_abbr go_dotenv "github.com/joho/godotenv"
+new_abbr go_envconfig "github.com/kelseyhightower/envconfig"
+new_abbr go_mongo "go.mongodb.org/mongo-driver/mongo"
+new_abbr go_resty "github.com/go-resty/resty/v2"
+new_abbr go_goquery "github.com/PuerkitoBio/goquery"
+
+# abbreviation path
 new_abbr "..." "../.."
 new_abbr "...." "../../.."
 new_abbr "....." "../../../.."
 new_abbr "......" "../../../../.."
 new_abbr "......." "../../../../../.."
+
+# TODO: add fisher plugin detection
+# jorgebucaran/fisher
+# pure-fish/pure
+# patrickf1/fzf.fish
+# catppuccin/fish
+# jorgebucaran/nvm.fish
+# jethrokuan/z
